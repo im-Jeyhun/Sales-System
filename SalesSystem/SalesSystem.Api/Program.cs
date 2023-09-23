ffd630aacb643f51005c2364238ba5326eed7959
Jeyhun <106176888+im-Jeyhun@users.noreply.github.com>
2023-09-23 10:04:36 +0000

    sometinh changed

--- SalesSystem/SalesSystem.Api/Program.cs
+++ SalesSystem/SalesSystem.Api/Program.cs
@@ -8,13 +8,11 @@ namespace SalesSystem.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.ConfigureServices(builder.Configuration);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             app.ConfigureMiddlewarePipeline();
 
             app.Run();
